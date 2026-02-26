3e3a80d2b79353750e24703756cea447dfad42e2
PedroReoli <pedrolucas2012@gmail.com>
2026-02-26 11:05:54 -0300

    a

--- src/api/appwrite.ts
+++ src/api/appwrite.ts
@@ -1,10 +1,19 @@
-import { Client, Account, Storage } from 'appwrite'
+import { Client, Account, Storage, Databases } from 'appwrite'
 
 const client = new Client()
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject('69a04328002453254a38')
 
+// Verifica conexão com o servidor Appwrite ao inicializar
+client.ping().then(() => {
+  console.log('[Appwrite] Conexão OK')
+}).catch((err) => {
+  console.warn('[Appwrite] Falha na conexão:', err)
+})
+
 export const account = new Account(client)
 export const storage = new Storage(client)
+export const databases = new Databases(client)
 
-export const BUCKET_ID = 'organon-stores'
+export const BUCKET_ID   = 'organon-stores'
+export const DATABASE_ID = 'organon-db'
